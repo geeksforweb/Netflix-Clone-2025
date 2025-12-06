2c7ac2741284441626e6d5e41d7f2307d137e78e
geeksforweb <getanehgashaw12@gmail.com>
2025-12-06 03:17:50 +0300

    change made on vite config file

--- Netflix-Clone/vite.config.js
+++ Netflix-Clone/vite.config.js
@@ -4,6 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // CRITICAL: Set the base path to your repository name
-  base: "/Netflix-Clone-2025/",
 });
